af273ab4ff8598d1e4b75c4ded274655c8d7e579
Vinayak Pawar <vinayak.pawar98765@gmail.com>
2026-09-12 22:09:59 +0530

    feat: add script to patch satellite.js module exports

--- scripts/patch-satellite.js
+++ scripts/patch-satellite.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const path = require('path');
-const file = path.join(__dirname, '../node_modules/satellite.js/lib/index.js');
-if (fs.existsSync(file)) {
-  let code = fs.readFileSync(file, 'utf8');
-  code = code.replace("export * as constants from './constants.js';", "import * as constants from './constants.js';\nexport { constants };");
-  fs.writeFileSync(file, code);
-  console.log('Patched satellite.js successfully');
-}
+const jsFile = path.join(__dirname, '../node_modules/satellite.js/dist/index.js');
+const dtsFile = path.join(__dirname, '../node_modules/satellite.js/dist/index.d.ts');
+
+[jsFile, dtsFile].forEach(file => {
+  if (fs.existsSync(file)) {
+    let code = fs.readFileSync(file, 'utf8');
+    code = code.replace("export * as constants from './constants.js';", "import * as constants from './constants.js';\nexport { constants };");
+    fs.writeFileSync(file, code);
+  }
+});
+console.log('Patched satellite.js successfully');
